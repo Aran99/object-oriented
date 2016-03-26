42684f108e2642cbd86a4259b2caa0b0b08d7d39
Day Day Up <Day Day Up>
2016-03-27 00:49:15 +0800

    code finish

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -2,13 +2,13 @@
 #include "scan.h"
 #include "print.h"
 using namespace std;
-extern int Flag;//调用外部变量 
+extern int Flag;//调用外部变量
 int main()
 {
 	Scan in;
 	Print out;
 	in.StringInput();
-	if(Flag)
+	if (Flag)
 		out.StringOutput();
 	return 0;
 }
--- calculator/print.cpp
+++ calculator/print.cpp
@@ -7,10 +7,10 @@ extern queue<string>q;
 void Print::StringOutput()
 {
 	int i,n;
-	n=q.size();
-	for(i=0;i<n;++i)
+	n = q.size();
+	for (i = 0; i < n; ++i)
 	{
-		cout<<q.front()<<endl;//输出队首的值 
-		q.pop();//将队首弹出 
-	}	
+		cout << q.front() << endl;//输出队首的值
+		q.pop();//将队首弹出
+	}
 }
